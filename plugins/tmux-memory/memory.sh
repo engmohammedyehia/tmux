65db91a3e795feee89536901e828391d1b854520
Mohammed Abdoh <engmohammedyehia@hotmail.com>
2019-10-22 18:07:02 +0200

    * Formatted the memory measurment

--- plugins/tmux-memory/memory.sh
+++ plugins/tmux-memory/memory.sh
@@ -7,6 +7,9 @@ main() {
   local ramIcon=$(_decode_unicode_escapes "${ram_icon-''}")
   local ramUnit="GB"
   local ram=$(top -l 1 | grep -E "^Phys" | grep -Eo "[0-9]+" | head -1)
+  if [ "$ram" -gt "1000" ]; then
+    ram=$((ram/1024))
+  fi
   printf "$ramIcon $ram$ramUnit"
 }
 
