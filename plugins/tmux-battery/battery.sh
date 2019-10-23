7102e86bb4cd2a79e8d67ed5791d670e0718cbe7
Mohammed Yehia <engmohammedyehia@hotmail.com>
2019-10-23 20:22:30 +0200

    * Added some styling to the battery plugin to indicate charging/need-charging statuses

--- plugins/tmux-battery/battery.sh
+++ plugins/tmux-battery/battery.sh
@@ -4,9 +4,22 @@ source $HOME/.tmux/plugins/helper.sh
 
 main() {
   local battery=$(pmset -g batt | grep -E "InternalBattery" | grep -Eo "[0-9]+%")
+  local batteryInt=$(pmset -g batt | grep -E "InternalBattery" | grep -Eo "[0-9]+%" | grep -Eo "[0-9]+")
   local battery_icon=$'\UF240 '
   local batteryIcon=$(_decode_unicode_escapes "${battery_icon-''}")
-  printf "#[fg=#212121,bg=#121212]#[fg=#8e8e8e,bg=#212121] $batteryIcon $battery%"
+  local batteryChargingStatus=$(pmset -g batt | grep -E "InternalBattery" | grep -Eo "; charging")
+  local batteryDisChargingStatus=$(pmset -g batt | grep -E "InternalBattery" | grep -E "discharging")
+  local batteryStatusChargingColor="#009900"
+  local batteryStatusDeadColor="#990000"
+  local batteryStatusToShowColor="#8e8e8e"
+  if [ "$batteryChargingStatus" ]; then
+    batteryStatusToShowColor="$batteryStatusChargingColor"
+  elif [ $batteryInt -lt 10 ]; then
+    batteryStatusToShowColor="$batteryStatusDeadColor"
+  else 
+    batteryStatusToShowColor="#8e8e8e"
+  fi
+  printf "#[fg=#212121,bg=#121212]#[fg=$batteryStatusToShowColor,bg=#212121] $batteryIcon #[fg=#8e8e8e,bg=#212121]$battery%"
 }
 
 main
