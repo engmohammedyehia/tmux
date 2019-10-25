1378ae3f507d6d09f529b2ab1274da749d0e4b5b
Mohammed Abdoh <engmohammedyehia@hotmail.com>
2019-10-25 14:18:24 +0200

    Revert "* Enhanced the Ram plugin by substracting the cached files value for more accurate measurement"
    This reverts commit 4890ef9910cc887073b9a739b119664d1612e311.

--- plugins/tmux-memory/memory.sh
+++ plugins/tmux-memory/memory.sh
@@ -7,14 +7,10 @@ main() {
   local ramIcon=$(_decode_unicode_escapes "${ram_icon-''}")
   local ramUnit="GB"
   local ram=$(top -l 1 | grep -E "^Phys" | grep -Eo "[0-9]+" | head -1)
-  local cachedFiles=$(top -l 1 | grep -E "^Phys" | grep -Eo "[0-9]+" | tail -1)
   if [ "$ram" -gt "1000" ]; then
     ram=$((ram/1024))
   fi
-  if [ "$cachedFiles" -gt "1000" ]; then
-    cachedFiles=$((cachedFiles/1024))
-  fi
-  printf "$ramIcon $(echo `expr $ram - $cachedFiles`)$ramUnit"
+  printf "$ramIcon $ram$ramUnit"
 }
 
 main
