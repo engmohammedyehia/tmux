28565cb5434f825e8f0d97d667db3f60a90b4c76
Mohammed Abdoh <engmohammedyehia@hotmail.com>
2019-10-25 14:20:31 +0200

    * Removed the bold formatting in the home plugin

--- plugins/tmux-home/home.sh
+++ plugins/tmux-home/home.sh
@@ -5,7 +5,7 @@ source $HOME/.tmux/plugins/helper.sh
 main() {
   local home_icon=$'\ue711'
   local homeIcon=$(_decode_unicode_escapes "${home_icon-''}")
-  printf "#[fg=colour232,bg=colour252,bold] $homeIcon #S"
+  printf "#[fg=colour232,bg=colour252,nobold] $homeIcon #S"
 }
 
 main
